696bd7503349a1ae533581feb6600d3388cb9dde
Abdel Raoof <olakara@gmail.com>
2025-03-10 22:33:53 +0400

    adding step 2

---
+++ chapter2/step2.go
@@ -0,0 +1,37 @@
+﻿package chapter2
+
+import "context"
+
+type LeadRequest struct {
+	Email string
+}
+
+type Lead struct {
+	ID string
+}
+
+type LeadCreator interface {
+	CreateLead(context context.Context, request LeadRequest) (Lead, error)
+}
+
+type Customer struct {
+	leadID string
+	userID string
+}
+
+func (c *Customer) UserID() string {
+	return c.userID
+}
+
+func (c *Customer) SetUserID(id string) {
+	c.userID = id
+}
+
+type LeadConverter interface {
+	Covert(ctx context.Context, subSelection SubscriptionType) (Customer, error)
+}
+
+func (l Lead) Convert(ctx context.Context, subSelection SubscriptionType) (Customer, error) {
+	// TODO implement conversion logic
+	panic("Covert Lead to Customer needs implementation!")
+}
