62f6fb2f9a5691c3e33a977d2e04f6d9c265829f
Abdel Raoof <olakara@gmail.com>
2025-03-10 22:25:58 +0400

    inital commit with preliminary design

---
+++ chapter2/preliminary.go
@@ -0,0 +1,50 @@
+﻿package chapter2
+
+import "context"
+
+type UserType = int
+type SubscriptionType = int
+
+const (
+	unknownUser UserType = iota
+	lead
+	customer
+	churned
+	lostLead
+)
+
+const (
+	unknownSubscription SubscriptionType = iota
+	basic
+	premium
+	exclusive
+)
+
+type UserAddRequest struct {
+	Email          string
+	SubType        SubscriptionType
+	UserType       UserType
+	PaymentDetails PaymentDetails
+}
+
+type UserModifyRequest struct {
+	ID             string
+	Email          string
+	SubType        SubscriptionType
+	UserType       UserType
+	PaymentDetails PaymentDetails
+}
+
+type User struct {
+	ID             string
+	PaymentDetails PaymentDetails
+}
+
+type PaymentDetails struct {
+	stripeTokenID string
+}
+
+type UserManager interface {
+	AddUser(ctx context.Context, request UserAddRequest) (User, error)
+	ModifyUser(ctx context.Context, request UserModifyRequest) (User, error)
+}
